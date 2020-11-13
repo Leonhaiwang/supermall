a111b5ccd29a9085f2eeaf9d3ad64d9537c713b5
Leonhaiwang <1379965820@qq.com>
2020-11-13 20:18:08 +0800

    laztload,px-to-vm

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Toast from 'components/common/Toast/index.js'
+import VueLazyLoad from 'vue-lazyload'
+import FastClick from 'fastclick'
 Vue.config.productionTip = false
 
+FastClick.attach(document.body)
+
+Vue.use(VueLazyLoad,{
+  loading:require('./assets/img/tabbar/category.svg')
+})
 Vue.prototype.$bus = new Vue()
 Vue.use(Toast)
 new Vue({
